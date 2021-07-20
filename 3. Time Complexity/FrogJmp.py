d16ff708fab2ecd9051584bbfc66ac7743bb511b
ayeshakhan <akhan7625@gmail.com>
2021-07-20 11:30:06 +0100

    Added questions to rest of solution files and did PermCheck question

--- 3. Time Complexity/FrogJmp.py
+++ 3. Time Complexity/FrogJmp.py
@@ -1,3 +1,13 @@
+def solution(X, Y, D):
+    b = Y - X
+    if Y == X:
+        return 0
+    if (b/D) == (b//D):
+        return b//D
+    else:
+        return b//D + 1
+    pass
+
 # A small frog wants to get to the other side of the road. The frog is currently located at position X and wants to get to a position greater than or equal to Y. The small frog always jumps a fixed distance, D.
 #
 # Count the minimal number of jumps that the small frog must perform to reach its target.
@@ -14,13 +24,3 @@
 #   Y = 85
 #   D = 30
 # the function should return 3, because the frog will be positioned as follows:
-
-def solution(X, Y, D):
-    b = Y - X
-    if Y == X:
-        return 0
-    if (b/D) == (b//D):
-        return b//D
-    else:
-        return b//D + 1
-    pass
